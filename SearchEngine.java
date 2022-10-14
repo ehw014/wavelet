54e0e2f9d99e7a852e1d5bbc338a39eea05aa586
ehw014 <35472009+ehw014@users.noreply.github.com>
2022-10-14 16:51:58 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 
 class Handler implements URLHandler {
     ArrayList<String> terms = new ArrayList<String>();
-
-    public String handleRequest(URI url)
-    {
+    public String handleRequest(URI url) {
         if (url.getPath().equals( "/")) 
             return terms.toString();
-        else
-        {
+        else {
             String[] parameters = url.getQuery().split("=");
             if (url.getPath().equals("/add")) {
                 terms.add(parameters[0]);
@@ -34,9 +31,7 @@ class SearchEngine {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-        
         int port = Integer.parseInt(args[0]);
-
         Server.start(port, new Handler());
     }
 }
